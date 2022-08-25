711049bb1d72895363af44364fc84f77a5e29445
RenzVillegas24 <renzjustinevillegas@gmail.com>
2022-08-25 21:53:48 +0800

    new animation when changing pages 	new file:   files/loader.svg not used

--- js/navSub.js
+++ js/navSub.js
@@ -1,9 +1,3 @@
-// scroll to top
-
-window.onbeforeunload = function () {
-	window.scrollTo(0, 0);
-}
-  
 
 function runOnScroll() {
 	const vh = window.innerHeight;
@@ -14,7 +8,6 @@ function runOnScroll() {
     const z = (window.scrollY) / vh;
     const a = Math.min(1,Math.max(z + (1-(vh-objh)/vh)*z,0));
 	const sz = (minvhvw/8);
-
 	
 	if (window.scrollY <= vh -objh) {
 		document.getElementById("nvImg").classList.remove('behindCllps');
@@ -22,36 +15,58 @@ function runOnScroll() {
 		document.getElementById("nvImg").style.filter = `blur(${10*(Math.max(a, 0.5)-0.5)*2}px)`;
 		document.querySelector(".header").style.height = `${objh-(objh*z)}%`;
 		document.getElementById("leftA").style.fontSize = `${(sz*(1-a)+(30*a))}pt`;
+		document.getElementById("leftB").style.fontSize = `${((sz/2)*(1-a)+(20*a))}pt`;
 		document.querySelector(".left").style.top = `${(85*(1-a))+((a)*50)}%`;
 		document.getElementById("header").classList.remove('heightNav');
 		document.getElementById("leftA").classList.remove('leftA2');
+		
+		document.querySelector(".right").style.transform = `translateY(${(-a*120)}%)`;
 		
 	} else if ((window.scrollY > vh -objh )){
 		document.getElementById("nvImg").classList.add('behindCllps');
 		document.getElementById("nvImg").style.opacity = `0`;
 		document.querySelector(".header").style.height = `100px`;
-		document.getElementById("leftA").style.fontSize = `${31	}pt`;
+		document.getElementById("leftA").style.fontSize = `31pt`;
+		document.getElementById("leftB").style.fontSize = `20pt`;
 		document.querySelector(".left").style.top = `50%`;
 		document.getElementById("header").classList.add('heightNav');
 		document.getElementById("leftA").classList.add('leftA2');
-
 	}
 	
 	document.querySelector(".main").style.paddingTop = `${vh}px`;
  }; 
-
+ 
  
 window.fadeTransition = function(href) {
     document.querySelector('body').style.opacity = 0
     setTimeout(function() { 
         window.location.href = href
-    }, 500)
+    }, 250)
 }
 
 
 window.addEventListener("scroll", runOnScroll);
 window.addEventListener('resize', runOnScroll);
-document.addEventListener("DOMContentLoaded",runOnScroll);
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1
-})
+document.addEventListener("DOMContentLoaded", runOnScroll);
+
+
+
+function onLoad(loading, loaded) {
+    if(document.readyState === 'complete'){
+        return loaded();
+    }
+    loading();
+    if (window.addEventListener) {
+        window.addEventListener('load', loaded, false);
+    }
+    else if (window.attachEvent) {
+        window.attachEvent('onload', loaded);
+    }
+};
+
+onLoad(function(){
+
+ },
+ function(){
+	document.querySelector('body').style.opacity = 1;
+ });
